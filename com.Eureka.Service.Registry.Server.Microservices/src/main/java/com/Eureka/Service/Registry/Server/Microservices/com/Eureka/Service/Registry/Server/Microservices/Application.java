1136cd2535e0c20728b67eb1f0c81fbe887f2ce0
Manish Kumar Sharma <78640018+Msharma673@users.noreply.github.com>
2024-08-20 21:32:28 +0530

    changes

--- com.Eureka.Service.Registry.Server.Microservices/src/main/java/com/Eureka/Service/Registry/Server/Microservices/com/Eureka/Service/Registry/Server/Microservices/Application.java
+++ com.Eureka.Service.Registry.Server.Microservices/src/main/java/com/Eureka/Service/Registry/Server/Microservices/com/Eureka/Service/Registry/Server/Microservices/Application.java
@@ -2,7 +2,9 @@ package com.Eureka.Service.Registry.Server.Microservices.com.Eureka.Service.Regi
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
--- com.User.Microservices/src/main/java/com/User/Microservices/com/User/Microservices/Application.java
+++ com.User.Microservices/src/main/java/com/User/Microservices/com/User/Microservices/Application.java
@@ -3,6 +3,8 @@ package com.User.Microservices.com.User.Microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Application {
 
